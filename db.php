b7310a36b806174c0769cda3b671a449cc68223c
tstark2 <todd2stark@gmail.com>
2015-04-14 22:08:07 -0500

    clean up

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=nurse911',
+    'username' => 'nurse',
+    'password' => '5wMTggvR%81F',
+    'charset' => 'utf8',
+];
