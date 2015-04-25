f94d65ccef2b4af225a7f18840a9c6253ad89e13
tstark2 <todd2stark@gmail.com>
2015-04-24 21:54:44 -0500

    Added Comments

--- nurse911/controllers/ConversionController.php
+++ nurse911/controllers/ConversionController.php
@@ -12,33 +12,37 @@ use app\models\Height;
 
 class ConversionController extends Controller
 {
+    //Controller for the Weight Conversions
     public function actionWeight()
     {
         $request = Yii::$app->request;
 
-        if($request->isPost) {
+        if($request->isPost) { //If information has been posted
+            //assign all the posted information to variables
             $quantity = $request->post('quantity');
             $unitFrom = $request->post('unitFrom');
             $unitTo = $request->post('unitTo');
             $labels = ['ounces', 'pounds', 'grams', 'kilograms'];
             $ufText = $labels[$unitFrom -1];
 
-            $weight = Weight::findById($unitFrom);
+            $weight = Weight::findById($unitFrom); //query the database for the conversion formulas
 
+            //convert the formula to a float from a string (MySQL doesn't store floats very well, using strings kept things accurate)
             $formula = floatval($weight->$unitTo);
 
-            $result = round(($quantity * $formula), 2);
+            $result = round(($quantity * $formula), 2); //round the result to two decimal places
 
-            $weightString = "$quantity $ufText equals $result $unitTo";
+            $weightString = "$quantity $ufText equals $result $unitTo"; //compile the results string
 
-            return $this->render('weight', ['result' => $weightString]);
+            return $this->render('weight', ['result' => $weightString]); //return it to the view
 
 
-        } else {
+        } else { //otherwise, just render the view
             return $this->render('weight');
         }
     }
 
+    //Controller for the Volume Conversions. Does exactly the same thing as the weight controller, just with the volume model.
     public function actionVolume()
     {
         $request = Yii::$app->request;
@@ -65,11 +69,13 @@ class ConversionController extends Controller
         }
     }
 
+    //I decided not to do temperatutre.
     /*public function actionTemperature()
     {
         return $this->render('temperature');
     }*/
 
+    //The controller for Height Conversions. Does exactly the same thing as the other two controllers, just to the height model.
     public function actionHeight()
     {
         $request = Yii::$app->request;
